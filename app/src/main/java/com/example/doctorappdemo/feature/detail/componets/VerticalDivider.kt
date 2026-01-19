a48841c0b569af75eda04f7e6827e040f30965f1
PQ-Thinh <thinh.pq.64cntt@ntu.edu.vn>
2026-01-19 09:25:49 +0700

    Components

---
+++ app/src/main/java/com/example/doctorappdemo/feature/detail/componets/VerticalDivider.kt
@@ -0,0 +1,20 @@
+package com.example.doctorappdemo.feature.detail.componets
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun VerticalDivider(color: Color) {
+    Box(modifier = Modifier
+        .width(1.dp)
+        .height(48.dp)
+        .background(color)
+    ) { }
+}
