d343f6773d6c0ef07f1a6a2e59755ee37ca19fb9
PQ-Thinh <thinh.pq.64cntt@ntu.edu.vn>
2026-01-19 09:26:12 +0700

    create Detail

---
+++ app/src/main/java/com/example/doctorappdemo/feature/detail/DetailHeader.kt
@@ -0,0 +1,65 @@
+package com.example.doctorappdemo.feature.detail
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import com.example.doctorappdemo.R
+
+@Composable
+fun DetailHeader(
+    pictureUrl: String,
+    onBack:()->Unit
+){
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .height(500.dp)
+        .background(colorResource(R.color.purple))
+        .statusBarsPadding()
+    ) {
+        IconButton(onClick = onBack,
+            modifier = Modifier
+                .align(Alignment.TopStart)
+                .padding(start = 16.dp,top= 8.dp)
+            )
+        {
+            Icon(painter = painterResource(R.drawable.back_white),
+                contentDescription = null,
+                tint = Color.Unspecified
+            )
+        }
+        IconButton(onClick = {},
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(end = 16.dp,top= 8.dp)
+        )
+        {
+            Icon(painter = painterResource(R.drawable.favorite_white),
+                contentDescription = null,
+                tint = Color.Unspecified
+            )
+        }
+        AsyncImage(
+            model = pictureUrl,
+            contentDescription = null,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .fillMaxSize()
+                .align(Alignment.TopCenter)
+        )
+    }
+}
---
+++ app/src/main/java/com/example/doctorappdemo/navigation/routes/DeltailRoute.kt
@@ -0,0 +1,78 @@
+package com.example.doctorappdemo.navigation.routes
+
+import android.content.Intent
+import android.net.Uri
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import com.example.doctorappdemo.core.model.DoctorModel
+import com.example.doctorappdemo.feature.detail.DetailScreen
+import com.example.doctorappdemo.navigation.Screen
+
+
+fun NavGraphBuilder.detailRoute(
+    nav: NavHostController,
+    onBack:()->Unit
+){
+    composable(Screen.Detail.route){
+        backStackEntry: NavBackStackEntry ->
+        val context = LocalContext.current
+        val prevEntry = remember(nav) {nav.previousBackStackEntry }
+        val doctor = remember(prevEntry) {
+            prevEntry?.savedStateHandle?.get<DoctorModel>("doctor")
+        }
+        LaunchedEffect(prevEntry,doctor) {
+            if(doctor==null){
+                onBack()
+            }else{
+                prevEntry?.savedStateHandle?.remove<DoctorModel>("doctor")
+            }
+        }
+        if (doctor!=null){
+            DetailScreen(
+                item = doctor,
+                onBack = onBack,
+                onOpenWebsite = {url->
+                    context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+                },
+                onSendSms = {mobile,body->
+                    context.startActivity(Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:$mobile"))
+                        .apply { putExtra("sms_body",body) }
+                    )
+                },
+                onDial = {mobile->
+                    context.startActivity(Intent(Intent.ACTION_DIAL, Uri.parse("tel:${mobile.trim()}")))
+                },
+                onDirection = {loc->
+                    context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=$loc")))
+                },
+                onShare = {subject, text ->
+                    context.startActivity(Intent.createChooser(
+                        Intent(Intent.ACTION_SEND)
+                            .apply {
+                                type = "text.plain"
+                                putExtra(Intent.EXTRA_SUBJECT,subject)
+                                putExtra(Intent.EXTRA_TEXT,text)
+
+                            },
+                        "Chia sẻ qua"
+                        )
+                    )
+                }
+            )
+        }else{
+            Spacer(Modifier.height(1.dp))
+        }
+
+
+    }
+
+}
